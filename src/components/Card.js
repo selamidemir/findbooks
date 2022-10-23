d3028b93f1dcae76b1e4e643d765c34fe5c5aec9
Selami Demir <selamidemir@gmail.com>
2022-10-23 18:09:37 +0300

    Arayüz düzenlemeleri yapıldı

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,9 +4,9 @@ function Card({book}) {
   return (
     <div className='card'>
       <div className='card-image'><img src={book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title} /></div>
-      <div className='card-title'><h3>{book.volumeInfo.title}</h3></div>
-      <div className='card-body'>{book.volumeInfo.subtitle}</div>
-      <div className='card-author'></div>
+      <h4 className='card-title'>{book.volumeInfo.title}</h4>
+      {/* <div className='card-body'>{book.volumeInfo.subtitle}</div> */}
+      <div className='card-author'>{book.volumeInfo.authors && book.volumeInfo.authors[0]}</div>
       <div className='card-action'></div>
     </div>
   )
--- src/components/Panel.js
+++ src/components/Panel.js
@@ -8,13 +8,15 @@ function Panel() {
   const { books, isLoading, error } = useBooks();
   console.log(books.length)
   return (
-    <div className="panel">
+    <div className="panel-cover">
+      <div className="panel">
       {isLoading && <Loading />}
       {error && <Error error={error} />}
       {!isLoading && 
         !error &&
         books.items &&
         books.items.map((book) => <Card book={book} key={book.id} />)}
+      </div>
     </div>
   );
 }
